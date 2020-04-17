45f5ce1c62604ab316bbdaa87128124bb6d6266c
boigraviz1107 <soccer.117@icloud.com>
2020-04-17 16:28:16 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200416094338) do
+ActiveRecord::Schema.define(version: 20200417062940) do
 
   create_table "incomes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.date     "year_month"
     t.integer  "value"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_incomes_on_user_id", using: :btree
+    t.integer  "user_id"
   end
 
   create_table "spendings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.date     "year_month"
     t.integer  "value"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_spendings_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -46,6 +43,4 @@ ActiveRecord::Schema.define(version: 20200416094338) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "incomes", "users"
-  add_foreign_key "spendings", "users"
 end
