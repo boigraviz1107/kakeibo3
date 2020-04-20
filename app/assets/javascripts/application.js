615479719e738c9cd3f180ac6a1ae11cd4d16dd7
boigraviz1107 <soccer.117@icloud.com>
2020-04-20 15:01:50 +0900

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,28 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree
 //= require turbolinks
-//= require_tree .
 //= require chartkick
 //= require Chart.bundle
+/**
+ * アコーディオン
+ *
+ * @auther ao-system
+ */
+(function(window,document){
+  var _toggleMenuButton = document.getElementById('toggleMenuButton');
+  var _toggleMenu = document.getElementById('toggleMenu');
+  var _clientH;
+   init();
+    function init() {
+      _toggleMenu.style.height = 'auto';
+      _clientH = _toggleMenu.clientHeight;
+      _toggleMenu.style.height = '0px';
+      _toggleMenuButton.addEventListener('click',function(){clickToggle();},false);
+  }
+   function clickToggle() {
+      var lastH = _toggleMenu.style.height;
+      _toggleMenu.style.height = (lastH == '' || lastH == '0px') ? _clientH + 'px' : '0px';
+  }
+})(window,document);
